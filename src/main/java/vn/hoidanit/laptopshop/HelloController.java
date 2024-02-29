1d81d1c4ee2ab635661672a621753612fcc9a144
VanDung <3ngk1sha@gmail.com>
2024-02-29 22:02:29 +0700

    test

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -2,14 +2,14 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.J;
 
 @RestController
 
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World!";
+        return "Hello World from Spring Boot!";
     }
 
 }
